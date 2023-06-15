6787cb8c09e393881acc0d505c25abc47f0760d0
Jean <jean102020023@gmail.com>
2023-06-15 14:18:42 +0800

    update add commodity

--- src/components/AddCommodity.vue
+++ src/components/AddCommodity.vue
@@ -228,15 +228,18 @@ button {
         width: 100%;
         height: 30%;
 
-
         .pic {
             width: 150px;
             height: 150px;
             margin-bottom: 70px;
 
             img {
-                width: 100%;
-                height: 100%;
+                max-width: 100%;
+                max-height: 100%;
+                width: auto;
+                height: auto;
+                display: block;
+                object-fit: contain;
             }
         }
 

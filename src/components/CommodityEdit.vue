c953161c500d4455ecc11ab02ce0f6516c15a94c
Jean <jean102020023@gmail.com>
2023-06-15 14:15:26 +0800

    update coomodityEdit

--- src/components/CommodityEdit.vue
+++ src/components/CommodityEdit.vue
@@ -298,15 +298,18 @@ button {
         width: 100%;
         height: 30%;
 
-
         .pic {
             width: 150px;
             height: 150px;
             margin-bottom: 70px;
 
             img {
-                width: 100%;
-                height: 100%;
+                max-width: 100%;
+                max-height: 100%;
+                width: auto;
+                height: auto;
+                display: block;
+                object-fit: contain;
             }
         }
 
@@ -315,5 +318,4 @@ button {
         }
 
     }
-}
-</style>
+}</style>
